058a0928eb27336ae46e4c4b36c52039d1381b4d
John Roswell Cua <ross.cua@gmail.com>
2020-01-27 15:12:08 -0800

    working webpack w/babe;

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,5 @@
-import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-function component() {
-  const element = document.createElement('div');
-
-  
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(<App />, document.getElementById('app'))
