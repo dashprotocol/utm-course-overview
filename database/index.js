7b4dbb30dc1dd07c87e59e81740875395ecfb7f4
John Roswell Cua <ross.cua@gmail.com>
2020-01-28 18:50:56 -0800

    updated database file

--- database/index.js
+++ database/index.js
@@ -24,3 +24,27 @@ let courseSchema = new mongoose.Schema({
     }]
   }
 })
+
+const Course = mongoose.model('Course', courseSchema);
+
+sampleData = [];
+
+for (let i = 0; i < 100; i++) {
+  sampleData.push({
+    course: {
+      course_name: faker.company.bs(),
+      sections: new Array(Math.floor(Math.random() * (15 - 5)) + 5).fill({
+        section_name: faker.company.catchPhrase(),
+        contents: [{
+          content_title: faker.company.bs(),
+          content_length: (Math.random() * 10),
+          is_vid: faker.Boolean(),
+          has_preview: faker.Boolean,
+          vid_img: faker.image.imageUrl()
+        }]
+      })
+    }
+  })
+}
+
+Course.insertMany(sampleData);
